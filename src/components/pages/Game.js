2a670d93c03d0960a76910e63272d3920a80874a
Amanda So <soamanda95@gmail.com>
2022-09-11 17:56:06 -0700

    add pokemon background image to game page

--- src/components/pages/Game.js
+++ src/components/pages/Game.js
@@ -1,9 +1,11 @@
 import React from "react";
+import pokemon from "../../assets/pokemon.png";
 
 const Game = () => {
     return (
         <div>
             <h1>Game Page</h1>
+            <img id="background" src={pokemon} alt={"pokemon"} ></img>
         </div>
 
     );
