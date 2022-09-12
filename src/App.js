5551c46dacb8d5abf64efcd895f64f9571eee7c3
Amanda So <soamanda95@gmail.com>
2022-09-11 17:42:06 -0700

    add link to game page from home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home';
 import Navbar from './components/pages/Navbar';
 import Leaderboard from './components/pages/Leaderboard';
+import Game from './components/pages/Game';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/game" element={<Game />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <h1>Home Page</h1>
+        <div>
+            <h1>Home Page</h1>
+            <button><Link to="/game">Start Game</Link></button>
+        </div>
+
     );
 };
 
