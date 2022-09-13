baf40a2b8f992c6e17c9d34785a9dcd70ce4e314
Amanda So <soamanda95@gmail.com>
2022-09-13 16:30:38 -0700

    disable select option when pokemon is found

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Dropdown = (props) => {
+    useEffect(() => {
+        let option = document.getElementById('pikachuOption');
+        if(props.pikachu === true) {
+            option.disabled = true
+        } else {
+            console.log("pikachu option still available")
+        }
+    }, [props.pikachu])
+    useEffect(() => {
+        let option = document.getElementById('munchlaxOption');
+        if(props.munchlax === true) {
+            option.disabled = true
+        } else {
+            console.log("munchlax option still available")
+        }
+    }, [props.munchlax])
+    useEffect(() => {
+        let option = document.getElementById('espeonOption');
+        if(props.espeon === true) {
+            option.disabled = true
+        } else {
+            console.log("espeon option still available")
+        }
+    }, [props.espeon])
     return (
         <div>
             <select onChange={props.onCheck} name="pokemon" id="pokemon">
                 <option value="none" disabled>Select Pokemon</option>
-                <option value="pikachu">Pikachu</option>
-                <option value="munchlax">Munchlax</option>
-                <option value="espeon">Espeon</option>
+                <option id="pikachuOption" value="pikachu">Pikachu</option>
+                <option id="munchlaxOption" value="munchlax">Munchlax</option>
+                <option id="espeonOption" value="espeon">Espeon</option>
             </select>
         </div>
 
