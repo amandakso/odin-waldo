ab7acb0e959a497b8e10ff2756ee65a97b1c8208
Amanda So <soamanda95@gmail.com>
2022-09-13 16:04:43 -0700

    add game heading with pokemon list

--- src/components/pages/Game.js
+++ src/components/pages/Game.js
@@ -5,7 +5,7 @@ import Dropdown from "../Dropdown";
 const Game = (props) => {
     return (
         <div>
-            <h1>Game Page</h1>
+            <h2 className="pokemonList">Find the following pokemon: <div><span id="pikachu">&#9683;</span>Pikachu</div><div><span id="munchlax">&#9683;</span>Munchlax</div><div><span id="espeon">&#9683;</span>Espeon</div><div id="message"></div></h2>
             <img id="background" src={pokemon} alt={"pokemon"} onClick={props.onClick}></img>
             <Dropdown onCheck={props.onCheck}/>
         </div>
