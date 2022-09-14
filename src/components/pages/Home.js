f8f56cdb8967073b01f4196b0291b2adbb76acf8
Amanda So <soamanda95@gmail.com>
2022-09-14 10:45:27 -0700

    add placeholder to indicate max characters allowed for player name

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,9 +5,8 @@ const Home = (props) => {
     return (
         <div>
             <h1>Home Page</h1>
-            <label htmlFor="playerName">Player Name: </label><input id="playerName" name="playerName" onChange={props.onChange}></input>
+            <label htmlFor="playerName">Player Name: </label><input id="playerName" name="playerName" placeholder="25 character max"onChange={props.onChange}></input>
             <button><Link to="/game">Start Game</Link></button>
-            <div>max 25 characters</div>
         </div>
 
     );
