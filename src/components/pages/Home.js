bb080e28798c4241e8a3810241cc94fd3a5d659e
Amanda So <soamanda95@gmail.com>
2022-09-14 10:43:53 -0700

    fix typo error for input label

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ const Home = (props) => {
     return (
         <div>
             <h1>Home Page</h1>
-            <label for="playerName">Player Name: </label><input id="playerName" name="playerName" onChange={props.onChange}></input>
+            <label htmlFor="playerName">Player Name: </label><input id="playerName" name="playerName" onChange={props.onChange}></input>
             <button><Link to="/game">Start Game</Link></button>
             <div>max 25 characters</div>
         </div>
