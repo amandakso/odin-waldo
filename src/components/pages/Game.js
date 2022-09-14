5e9be9ba8f004b63337e1b62dfdbd508f093ee64
Amanda So <soamanda95@gmail.com>
2022-09-13 18:10:11 -0700

    add messages when pokemon are found

--- src/components/pages/Game.js
+++ src/components/pages/Game.js
@@ -5,28 +5,41 @@ import Dropdown from "../Dropdown";
 const Game = (props) => {
     useEffect(() => {
         let pikachu = document.getElementById('pikachu');
+        let message = document.getElementById('message');
         if(props.pikachu === true) {
             pikachu.style.display = "inline-block";
+            message.innerText = "You Found Pikachu! Find the remaining pokemon!"
         } else {
             pikachu.style.display = "none";
         }
     }, [props.pikachu])
     useEffect(() => {
         let munchlax = document.getElementById('munchlax');
+        let message = document.getElementById('message');
         if(props.munchlax === true) {
             munchlax.style.display = "inline-block";
+            message.innerText = "You Found Munchlax! Find the remaining pokemon!"
         } else {
             munchlax.style.display = "none";
         }
     }, [props.munchlax])
     useEffect(() => {
         let espeon = document.getElementById('espeon');
+        let message = document.getElementById('message');
         if(props.espeon === true) {
             espeon.style.display = "inline-block";
+            message.innerText = "You Found Espeon! Find the remaining pokemon!"
         } else {
             espeon.style.display = "none";
         }
     }, [props.espeon])
+
+    useEffect(() => {
+        let message = document.getElementById('message');
+        if (props.pikachu && props.munchlax && props.espeon) {
+            message.innerText = 'All pokemon have been found!';
+        }
+    }, [props.pikachu, props.munchlax, props.espeon])
 
 
     return (
