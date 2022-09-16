d5a2ab6c40d6f064b33d46b4e0b7dbf427cacb69
Amanda So <soamanda95@gmail.com>
2022-09-15 18:46:36 -0700

    update info on homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,8 @@ const Home = (props) => {
 
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Where's Waldo: Pokemon Edition</h1>
+            <p>Catch the 3 listed pokemon as fast as you can! Check out the leaderboard to see the fastest times!</p>
             <label htmlFor="playerName">Player Name: </label><input id="playerName" name="playerName" placeholder="25 character max"onChange={props.onChange}></input>
             <button onClick={props.setUp}><Link to="/game" id="start" >Start Game</Link></button>
         </div>
